94e4898c486868da224ec6bda3dff705e57e9f33
Asa Brouet <asa.brouet@gmail.com>
2024-09-22 15:17:48 -0400

    Added

--- android/src/main/java/com/example/zebra_rfid_sdk_plugin/ZebraRfidSdkPlugin.java
+++ android/src/main/java/com/example/zebra_rfid_sdk_plugin/ZebraRfidSdkPlugin.java
@@ -25,6 +25,7 @@ public class ZebraRfidSdkPlugin implements FlutterPlugin, MethodCallHandler, Str
   private MethodChannel channel;
   private EventChannel eventChannel;
   private RFIDHandler rfidHandler;
+  private static ArrayList<ReaderDevice> readersListArray;
   private Context context;
   private EventChannel.EventSink sink = null;
 
@@ -47,7 +48,15 @@ public class ZebraRfidSdkPlugin implements FlutterPlugin, MethodCallHandler, Str
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     switch (call.method) {
       case "getPlatformVersion":
-        result.success("Android " + android.os.Build.VERSION.RELEASE);
+        readersListArray =  rfidHandler.getReadersList();
+        if (readersListArray.size() > 0) {
+          readerDevice = readersListArray.get(0);
+          reader = readerDevice.getRFIDReader();
+          result.success("Android " + reader.getHostName());
+        } else {
+          result.success("Android " + 'nothing');
+        }
+
         break;
       case "toast":
         String txt=call.argument("text");
