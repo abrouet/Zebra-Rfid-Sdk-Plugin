b35f78ada01aad3a04bb2ec1986c63523e50a530
Asa Brouet <asa.brouet@gmail.com>
2024-09-22 15:26:16 -0400

    Added

--- android/src/main/java/com/example/zebra_rfid_sdk_plugin/ZebraRfidSdkPlugin.java
+++ android/src/main/java/com/example/zebra_rfid_sdk_plugin/ZebraRfidSdkPlugin.java
@@ -13,6 +13,8 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import com.zebra.rfid.api3.RFIDReader;
+import com.zebra.rfid.api3.ReaderDevice;
 
 /**
  * ZebraRfidSdkPlugin
